795b57c7ce97077256a72ce1f52de62486e608fd
gsscoder <gsscoder@gmail.com>
2013-02-23 16:46:21 +0100

    Adding NancyFx demo to project tree.

---
+++ src/Owin.Listener.Demo.NancyFx/HelloWorldModule.cs
@@ -0,0 +1,14 @@
+﻿namespace Owin.Listener.Demo.NancyFx
+{
+    using Nancy;
+
+    public class HelloWorldModule : NancyModule
+    {
+        public HelloWorldModule()
+        {
+            Get["/"] = _ => "<a href=\"/hello\">go here</a>";
+
+            Get["/hello"] = _ => "Hello, Nancy OWIN!";
+        }
+    }
+}
---
+++ src/Owin.Listener.Demo.NancyFx/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Owin.Listener.Demo.NancyFx
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Nancy;
+    using Nancy.Owin;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nancyOwin = new NancyOwinHost(EmptyApp, new DefaultNancyBootstrapper());
+
+            var listener = new OwinHttpListener(nancyOwin.Invoke, new IPAddress(new byte[] { 0, 0, 0, 0 }), 8899);
+
+            listener.Start();
+            listener.ListenAsync().Wait();
+
+            Console.ReadLine();
+
+            listener.Stop();
+        }
+
+        static Task EmptyApp(IDictionary<string, object> env)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            tcs.SetResult(null);
+            return tcs.Task;
+        }
+    }
+}
